f8223e240d32493b4412a06d4d2956098ddaab33
Charlotte <2475496+chworthey@users.noreply.github.com>
2024-07-07 17:28:57 -0600

    Burger menu

--- src/main/components/Menubar.tsx
+++ src/main/components/Menubar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import studioLogo from "/cairns.gif";
 import { ToggleSwitch } from "./ToggleSwitch";
 import { IMenuItem } from "../types/IMenuItem";
+import { useState } from "react";
 
 interface IMenuBarProps {
   IsPartyModeOn: boolean;
@@ -12,17 +13,26 @@ interface IMenuBarProps {
 export function Menubar(props: IMenuBarProps) {
   const linkClass = props.IsPartyModeOn ? "a__navbar__page a__navbar__page--party-mode" : "a__navbar__page";
 
+  const [burgerMenuActive, setBurgerMenuActive] = useState(false);
+
   return (
-    <div className="div__nav-container">
-      <nav className="nav__navbar">
+    <nav className="nav__navbar">
+      <div className="div__menu-top-items">
         <Link className={props.IsPartyModeOn ? 
           "a__logo-title a__logo-title--party-mode" : "a__logo-title"} to="/">
-          <span>Worthey Studios</span>
-          <img className="img__logo" src={studioLogo} alt="An animation depicting stones falling onto a stack."></img>
+          Worthey Studios
+          {/* <img className="img__logo" src={studioLogo} alt="An animation depicting stones falling onto a stack."></img> */}
         </Link>
+        <div className="div__burger-menu" onClick={() => setBurgerMenuActive(!burgerMenuActive)}>
+          <div className="div__burger-menu-line"/>
+          <div className="div__burger-menu-line"/>
+          <div className="div__burger-menu-line"/>
+        </div>
+      </div>
+      <div className={burgerMenuActive ? "div__menu-items" : "div__menu-items div__menu-items--hidden"}>
         {props.Items.map((i, index) => <Link key={index} className={linkClass} to={i.LinkTo}>{i.LinkText}</Link>)}
-      </nav>
-      <ToggleSwitch LabelText="Party Mode Toggle" On={props.IsPartyModeOn} OnClick={props.OnPartyModeSet}/>
-    </div>
+        <ToggleSwitch LabelText="Party Mode Toggle" On={props.IsPartyModeOn} OnClick={props.OnPartyModeSet}/>
+      </div>
+    </nav>
   );
 }
