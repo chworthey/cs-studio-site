bdfb84401da44bf5230128fbd4aa6afe43ab27ea
Charlotte <chworthey@gmail.com>
2024-07-15 10:28:37 -0600

    Strange site errors, hardcoded config

--- src/portal/main/types/Backend.ts
+++ src/portal/main/types/Backend.ts
@@ -1,5 +1,6 @@
+const endpoint = 'https://wortheystudiosapi.azurewebsites.net';
+
 export function SendMessage(body: string, subject: string | undefined, important: boolean, onComplete: (success: boolean) => void) {
-  const endpoint = import.meta.env.VITE_BACKEND_ENDPOINT as string;
   const url = subject ? `${endpoint}/api/message?subject=${subject}` : `${endpoint}/message`; 
   fetch(url, {
     method: 'POST',
