6f22c2bb97c58e9d4066318d09a58837eb1895e4
Charlotte <2475496+chworthey@users.noreply.github.com>
2025-06-14 12:00:20 -0600

    Revert "Revert "Updated api endpoint""
    This reverts commit 89910e67d11605043871e2280337e483a477eb7e.

--- src/main/components/App.tsx
+++ src/main/components/App.tsx
@@ -110,7 +110,7 @@ export function App() {
       <div ref={appRef} className="div__app" role="presentation">
         <RouterProvider router={router}/>
       </div>
-      <Warning Title="Heads up!" Text="6/12/2025: Student sign-ups are not working right now... Azure is down... Please check back later. Thanks. ~Charlotte"/>
+      {/* <Warning Title="Heads up!" Text="6/12/2025: Student sign-ups are not working right now... Azure is down... Please check back later. Thanks. ~Charlotte"/> */}
       {nekoShown && <NekoOverlay IsTouchScreen={isTouchScreen()}/>}
     </div>
   );
